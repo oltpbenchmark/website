0346b58cf81fb19febee08d1e89ed7c61a745fb0
Andy Pavlo <pavlo@cs.brown.edu>
2016-09-07 15:32:50 -0400

    Updated wsgi.py to automatically work with Apache

--- website/wsgi.py
+++ website/wsgi.py
@@ -1,34 +1,16 @@
 """
-WSGI config for sample project.
-
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
-
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
+WSGI config for OtterTune
 """
-import os, sys
 
-sys.path.append('/var/www/oltpbench/website')
+import os
+import sys
 
-# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
-# if running multiple sites in the same mod_wsgi process. To fix this, use
-# mod_wsgi daemon mode with each site in its own daemon process, or use
-# os.environ["DJANGO_SETTINGS_MODULE"] = "website.settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
+# Change the env variable where django looks for the settings module
+# http://stackoverflow.com/a/11817088
+import django.conf
+django.conf.ENVIRONMENT_VARIABLE = "DJANGO_OTTERTUNE_SETTINGS_MODULE"
+os.environ.setdefault("DJANGO_OTTERTUNE_SETTINGS_MODULE", "website.settings")
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
